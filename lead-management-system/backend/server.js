d1f1091fa56b571ff6a474193e3cc53a5a5ec38e
morebo1 <sanketmore.39410@gamil.com>
2026-04-29 21:56:03 +0530

    fix deployment issues

--- lead-management-system/backend/server.js
+++ lead-management-system/backend/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const leadRoutes = require("./routes/leadRoutes");
-
-const app = express();
 const pool = require("./db");
 
-app.use(cors());
+const app = express();
+
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
 
 // Routes
@@ -15,12 +18,13 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-
+// DB Connection Test
 pool.connect()
   .then(() => console.log("PostgreSQL Connected"))
   .catch(err => console.error("Connection Error", err));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
